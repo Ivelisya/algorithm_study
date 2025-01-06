7bf6ef8d527a382a4bd5c6062948961b90ae9616
smazyu <138857469+Ivelisya@users.noreply.github.com>
2025-01-06 11:28:25 +0800

    Update 最后一个单词的长度.cpp

--- AlgorithmExercises/day_10/最后一个单词的长度.cpp
+++ AlgorithmExercises/day_10/最后一个单词的长度.cpp
@@ -5,4 +5,19 @@
 子字符串
 
 。
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        int length = 0;
+        while(i >= 0 && s[i] ==' '){
+            i --;
+        }
+        while(i >= 0 && s[i] != ' '){
+            length ++;
+            i --;
+        }
+        return length;
+    }
+};
 #endif
