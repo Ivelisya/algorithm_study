786c9f4abacd2f58695e9474a8bf7a72c5e2291a
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-05-06 22:26:12 +0800

    初始化字符串排列问题的解决方案框架

---
+++ LeetCode/567.字符串的排列.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=567 lang=cpp
+ *
+ * [567] 字符串的排列
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool checkInclusion(string s1, string s2)
+    {
+        
+    }
+};
+// @lc code=endLeetCode/567.字符串的排列.cpp
